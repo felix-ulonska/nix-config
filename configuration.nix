3cfe28aaaaeeec942e019a01cfa8f9e6175d18cd
Felix Ulonska <f.ulonska@devsaur.com>
2022-03-10 01:03:24 +0100

    update

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda";
 
-  networking.hostName = "cool-server";
+  networking.hostName = "nixos";
 
   time.timeZone = "Europe/Amsterdam";
 
@@ -20,6 +20,9 @@
     isNormalUser = true;
     extraGroups = [ "wheel" ];
   };
+  users.users."root".openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGQ+BFtjE8D9+wVAnZ7IrhkTPlA62jdEq037+PaKCXkM jabbi@mimo"
+  ];
 
   environment.systemPackages = with pkgs; [
     vim
@@ -27,6 +30,7 @@
   ];
 
   services.openssh.enable = true;
+  services.openssh.permitRootLogin = "yes";
 
   networking.firewall.allowedTCPPorts = [ 22 ];
   networking.firewall.allowedUDPPorts = [ 22 ];
--- flake.nix
+++ flake.nix
@@ -3,13 +3,31 @@
 
   inputs = {
     nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    deploy-rs = {
+      url = "github:serokell/deploy-rs";
+    };
   };
-  outputs = inputs: {
-    cool-system = inputs.nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, deploy-rs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
       ];  
+    };
+    deploy = {
+      sshUser = "root";           # SSH login username
+      user = "root";              # Remote username
+      nodes = {
+        "nixos" = {
+          # Target node's address, either IP, domain, or .ssh/config alias
+          hostname = "5.45.111.134";
+          profiles.system = {
+            # Use nixosConfigurations."nixos" defined above
+            path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations."nixos";
+          };
+        };
+      };
+      checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
     };
   };
 }
