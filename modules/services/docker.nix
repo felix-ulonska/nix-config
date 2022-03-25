f74c73be057e614e3a70156f5ed1f47c40dd42a8
Felix Ulonska <f.ulonska@devsaur.com>
2022-03-25 13:17:01 +0100

    removed useless files

--- modules/services/docker.nix
+++ modules/services/docker.nix
@@ -11,9 +11,9 @@ in
     virtualisation.podman.enable = true;
     virtualisation.podman.dockerSocket.enable = true;
     virtualisation.podman.defaultNetwork.dnsname.enable = true;
-    virtualisation.oci-containers.backend = "docker";
+    virtualisation.oci-containers.backend = "podman";
 
     # Use your username instead of `myuser`
-    users.extraUsers.jabbi.extraGroups = [ "docker" ];
+    users.extraUsers.jabbi.extraGroups = [ "podman" ];
   };
 }
