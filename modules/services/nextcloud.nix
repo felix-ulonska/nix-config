82fed665fa724179d77d081e53e9d936e791fa71
Felix Ulonska <git@webfoo.de>
2024-10-31 01:03:55 +0100

    bump nextcloud 30

--- modules/services/nextcloud.nix
+++ modules/services/nextcloud.nix
@@ -27,7 +27,7 @@ in
     services.nextcloud = {
       enable = true;
       hostName = "cloud.webfoo.de";
-      package = pkgs.nextcloud29;
+      package = pkgs.nextcloud30;
 
       # Use HTTPS for links
       https = true;
