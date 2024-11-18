9dcb56e6ca4b4ba03404ddb6001cf37d54709d41
Felix Ulonska <git@webfoo.de>
2024-11-18 23:14:14 +0100

    feat: power management

--- hosts/the-bird/configuration.nix
+++ hosts/the-bird/configuration.nix
@@ -29,6 +29,29 @@
     HandlePowerKey=ignore
   '';
 
+  powerManagement = {
+    enable = true;
+    powertop.enable = true;
+    cpuFreqGovernor = "powersave";
+  };
+  services = {
+    thermald.enable = true;
+    power-profiles-daemon.enable = false;
+    auto-cpufreq = {
+      enable = true;
+      settings = {
+        battery = {
+          governor = "powersave";
+          turbo = "never";
+        };
+        charger = {
+          governor = "powersave";
+          turbo = "auto";
+        };
+      };
+    };
+  };
+
   nixpkgs.config.permittedInsecurePackages = [
     "electron-25.9.0"
   ];
